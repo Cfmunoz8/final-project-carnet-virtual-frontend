409e999ea6480ca24870333fe3965ac17e912c6d
TrinidadMartiG <trinidad.martig@gmail.com>
2022-11-19 01:32:58 +0000

    added Login/Registration view

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -4,6 +4,8 @@ import injectContext from "./store/context";
 import Home from "./views/Home";
 import PatientListProfessional from "./views/PatientListProfessional";
 import NewControl from "./views/NewControl";
+import LoginView from "./views/LoginView";
+import RegistrationView from "./views/RegistrationView";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/patient-list" element={<PatientListProfessional />} />
         <Route path="/new-control" element={<NewControl />} />
+        <Route path="/LoginView" element={<LoginView />} />
+        <Route path="/RegistrationView" element={<RegistrationView />} />
       </Routes>
     </BrowserRouter>
   );
--- final-project/src/views/Home.js
+++ final-project/src/views/Home.js
@@ -1,9 +1,17 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
 
 function Home() {
   return (
     <div className="home">
       <h1>soy el home</h1>
+      <Link to='./RegistrationView' >
+        <button className="btn btn-primary"> Sign in </button>
+      </Link>
+      <Link to='./LoginView' >
+        <button className="btn btn-primary"> Log in </button>
+      </Link>
     </div>
   );
 }
