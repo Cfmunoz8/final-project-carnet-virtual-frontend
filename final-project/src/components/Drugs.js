857e40ee544ae19c1d6b8ce340afdb057032d923
Cfmunoz8 <cfmunoz8@gmail.com>
2022-11-23 22:39:00 +0000

    component drugs

---
+++ final-project/src/components/Drugs.js
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/context";
+
+function Drugs() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+  
+  export default Drugs;
+  
