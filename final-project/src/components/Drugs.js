1270a9a5f89b34cff960f9f82d35c8a96d299e50
Cfmunoz8 <cfmunoz8@gmail.com>
2022-12-04 23:22:33 +0000

    post drugs

--- final-project/src/components/Drugs.js
+++ final-project/src/components/Drugs.js
@@ -1,26 +1,73 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/context";
 
 function Drugs() {
+  const { store, actions } = useContext(Context);
+  const [drugsAndPosology, setDrugsAndPosology] = useState();
+
+  useEffect(() => {
+    actions.getDrugs();
+  }, []);
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setDrugsAndPosology({... drugsAndPosology, [e.target.name] : e.target.value})
+  };
+
+  const submit = (e) => {
+    actions.addDrug(drugsAndPosology, e)
+  };
+
   return (
     <div>
-      <div className="container container-fluid mt-3 mb-3 p-3 bg-light">
-        <div className="row">
-          <div className="col mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">
-              Medicamentos:
-            </label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-            ></textarea>
-            <button type="button" className="btn btn-info">
-              Guardar
-            </button>
-          
+      <div className="row pb-3">
+        <div className="col mb-3">
+          <div className="card">
+            <div className="card-header">Medicamentos</div>
+            <ul className="list-group list-group-flush list-group-numbered">
+              {store.drugs?.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className="list-group-item d-flex justify-content-between align-items-start"
+                  >
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold">
+                        {item.name} - {item.posology}
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
+        <form onSubmit={()=>{submit()}}>
+          <div className="input-group">
+            <span className="input-group-text">Agregar un medicamento: </span>
+            <input
+              type="text"
+              aria-label="Drug"
+              className="form-control"
+              placeholder="Fármaco"
+              onChange={(e) => {
+                onChange(e);
+              }}
+            />
+            <input
+              type="text"
+              aria-label="Posology"
+              className="form-control"
+              placeholder="Posología"
+              onChange={(e) => {
+                onChange(e);
+              }}
+            />
+          </div>
+          <button type="submit" className="btn btn-info">
+            Guardar
+          </button>
+        </form>
       </div>
     </div>
   );
--- final-project/src/store/store.js
+++ final-project/src/store/store.js
@@ -27,6 +27,34 @@ export const getState = ({ getActions, getStore, setStore }) => {
           .then((result) => console.log(result))
           .catch((error) => console.error("Error:", error));
       },
+      
+      
+      getDrugs: () => {
+        fetch(
+          "https://8080-4geeksacademy-htmlhello-l349w1sqq6b.ws-us77.gitpod.io/drugs"
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setStore({ drugs: data });
+          });
+      },
+
+      addDrug: (drugAndPosology) => {
+        const postMethod = {
+          method: "POST",
+          body: JSON.stringify(drugAndPosology),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        fetch(
+          "https://8080-4geeksacademy-htmlhello-l349w1sqq6b.ws-us77.gitpod.io/create_drug/",
+          postMethod
+        )
+          .then((res) => res.json())
+          .then((result) => console.log (result))
+          .catch((error) => console.error("Error:", error));
+      }
     },
   };
 };
